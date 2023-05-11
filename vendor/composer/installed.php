93a5095ffc12def3f2e64ae6c27810c86ded9519
picocodes <picocodes@gmail.com>
2023-05-11 15:14:12 +0300

    fix phpcs errors

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2ecb1ff5a496697afc332dbbc2601034ddc245a8',
+        'name' => 'hizzle/forms',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hizzle/forms' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2ecb1ff5a496697afc332dbbc2601034ddc245a8',
+            'dev_requirement' => false,
+        ),
+    ),
+);
