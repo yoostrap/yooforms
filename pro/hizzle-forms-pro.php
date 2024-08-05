3063ffe7351aa0b0967b50443c5bf986252f5fdc
Lewis Ushindi <frenziecodes@gmail.com>
2024-08-05 18:37:15 +0300

    tweak

--- pro/hizzle-forms-pro.php
+++ pro/hizzle-forms-pro.php
@@ -9,7 +9,10 @@ class Hizzle_Forms_pro {
      */
     public function __construct() {
         if (is_admin()) {
-            // Ensure that WP_List_Table is loaded before the Entries class is used
+            // Ensure that template.php and class-wp-list-table.php are loaded before the Entries class is used
+            if (!function_exists('convert_to_screen')) {
+                require_once ABSPATH . 'wp-admin/includes/template.php';
+            }
             if (!class_exists('WP_List_Table')) {
                 require_once ABSPATH . 'wp-admin/includes/class-wp-list-table.php';
             }
